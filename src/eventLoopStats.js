0e126ccf03ac2022ac3cd625718a2cd781aa1ee9
netstorm <netstorm@info@cavisson.com>
2016-11-23 19:13:53 +0530

    first commit

---
+++ src/eventLoopStats.js
@@ -0,0 +1,10 @@
+var pack = require('../package.json');
+var eventLoopStats = require('./' + [
+    'build',
+    'eventLoopStats',
+    'v' + pack.version,
+    ['node', 'v' + process.versions.modules, process.platform, process.arch].join('-'),
+    'eventLoopStats.node'
+  ].join('/'));
+
+exports.sense = eventLoopStats.sense;
